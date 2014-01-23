713e911db3d6e299d8e79d2435f0252b2dc9667a
Jeremy Crawford <jcrawford.design@gmail.com>
2014-01-23 03:52:30 -0500

    Initial dist subtree commit

---
+++ app/scripts/main.js
@@ -0,0 +1,9 @@
+
+var house = {
+	value: 100,
+	color: 'white',
+	windows: 6,
+	appreciates: function(){
+		console.log('I appreciated in value');
+	}
+}
---
+++ dist/scripts/90b4dce7.main.js
@@ -0,0 +1,1 @@
+var house={value:100,color:"white",windows:6,appreciates:function(){console.log("I appreciated in value")}};
